3f104a6b4319832aedcb86fb28d6c5f71aa7ea33
kellar <blakekellar@gmail.com>
2016-05-15 11:51:07 -0500

    Adds matrix cache function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R provides a matrix inverse cache.
+##
+## Example usage:
+## > theMatrix <- makeCacheMatrix(matrix(c(1,2,3,4),nrow=2,ncol=2))
+## > cacheSolve(theMatrix)
+##     [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(theMatrix)
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Defines a cacheAugmentedMatrix object which contains a matrix and its
+## inverse. Setter and getter functions are returned in a list.
+makeCacheMatrix <- function(basicRMatrix = matrix()) {
+  inverse <- NULL
+  setMatrix <- function(newBasicRMatrix) {
+    basicRMatrix <<- newBasicRMatrix
+    inverse <<- NULL
+  }
+  getMatrix <- function() basicRMatrix
+  setInverse <- function(newInverse) inverse <<- newInverse
+  getInverse <- function() inverse
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function returns the inverse of a cacheAugmentedMatrix object
+## (see makeCacheMatrix). If the inverse of this cacheAugmentedMatrix has
+## already been computed it returns that cached inverse, else it computes,
+## caches, and returns the inverse.
+cacheSolve <- function(cacheAugmentedMatrix, ...) {
+  ## Return a matrix that is the inverse of 'cacheAugmentedMatrix'
+  inverse <- cacheAugmentedMatrix$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- cacheAugmentedMatrix$getMatrix()
+  inverse <- solve(data, ...)
+  cacheAugmentedMatrix$setInverse(inverse)
+  inverse
 }
+
